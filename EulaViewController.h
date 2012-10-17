55024c331481f9df84977de968f500b40b30a49e
bietz <dan.bietz@tee2.org>
2012-10-17 13:09:06 -0700

    batch commit

--- EulaViewController.h
+++ EulaViewController.h
@@ -5,7 +5,35 @@
 //  Created by Brian Doherty on 6/21/12.
 //  Copyright (c) 2012 T2. All rights reserved.
 //
-
+/*
+ *
+ * Provider Resilience
+ *
+ * Copyright © 2009-2012 United States Government as represented by
+ * the Chief Information Officer of the National Center for Telehealth
+ * and Technology. All Rights Reserved.
+ *
+ * Copyright © 2009-2012 Contributors. All Rights Reserved.
+ *
+ * THIS OPEN SOURCE AGREEMENT ("AGREEMENT") DEFINES THE RIGHTS OF USE,
+ * REPRODUCTION, DISTRIBUTION, MODIFICATION AND REDISTRIBUTION OF CERTAIN
+ * COMPUTER SOFTWARE ORIGINALLY RELEASED BY THE UNITED STATES GOVERNMENT
+ * AS REPRESENTED BY THE GOVERNMENT AGENCY LISTED BELOW ("GOVERNMENT AGENCY").
+ * THE UNITED STATES GOVERNMENT, AS REPRESENTED BY GOVERNMENT AGENCY, IS AN
+ * INTENDED THIRD-PARTY BENEFICIARY OF ALL SUBSEQUENT DISTRIBUTIONS OR
+ * REDISTRIBUTIONS OF THE SUBJECT SOFTWARE. ANYONE WHO USES, REPRODUCES,
+ * DISTRIBUTES, MODIFIES OR REDISTRIBUTES THE SUBJECT SOFTWARE, AS DEFINED
+ * HEREIN, OR ANY PART THEREOF, IS, BY THAT ACTION, ACCEPTING IN FULL THE
+ * RESPONSIBILITIES AND OBLIGATIONS CONTAINED IN THIS AGREEMENT.
+ *
+ * Government Agency: The National Center for Telehealth and Technology
+ * Government Agency Original Software Designation: Provider Resilience001
+ * Government Agency Original Software Title: Provider Resilience
+ * User Registration Requested. Please send email
+ * with your contact information to: robert.kayl2@us.army.mil
+ * Government Agency Point of Contact for Original Software: robert.kayl2@us.army.mil
+ *
+ */
 #import <UIKit/UIKit.h>
 
 @interface EulaViewController : UIViewController <UIAlertViewDelegate> {
---
+++ README.md
@@ -0,0 +1,31 @@
+Provider Resilience
+===============
+
+[Official Site: http://www.t2health.org](http://t2health.org)
+
+
+Submitting bugs
+===============
+If you think you've found a bug, please report it by following these instructions:  
+
+1. Visit the [Issue tracker: https://github.com/t2health/ProviderResilience-iOS/issues](https://github.com/t2health/ProviderResilience-iOS/issues)
+2. Create an issue explaining the problem and expected result
+    - Be sure to include any relevant information for reproducing the issue
+    - Include information such as:
+        * Device (with version #)
+        * The version of code you're running
+        * If you are running from a git version, include the date and/or hash number
+3. Submit the issue.
+
+Submitting patches
+==================
+To contribute code and bug fixes to Provider Resilience: fork this project on Github, make changes to the code in your fork, 
+and then send a "pull request" to notify the team of updates that are ready to be reviewed for inclusion.
+
+Detailed instructions can be found at [Patching](https://gist.github.com/1507418)
+
+License
+==============================================
+Copyright 2012 The National Center for Telehealth and Technology
+
+Provider Resilience is Licensed under the NASA Open Source License: [https://github.com/t2health/ProviderResilience-iOS/blob/master/NASA-LICENSE.txt](https://github.com/t2health/ProviderResilience-iOS/blob/master/NASA-LICENSE.txt)
